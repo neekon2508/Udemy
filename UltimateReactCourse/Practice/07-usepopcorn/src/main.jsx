75de0821d706ef11122a54780eb60c2262bebc47
neekon2508 <nghia250803@gmail.com>
2025-10-08 14:25:28 +0700

    Ultimate React Course: complete section 10

--- UltimateReactCourse/Practice/07-usepopcorn/src/main.jsx
+++ UltimateReactCourse/Practice/07-usepopcorn/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+import StarRating from "./StarRating.jsx";
 
-createRoot(document.getElementById('root')).render(
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Test />
+  </StrictMode>
+);
